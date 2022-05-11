1d6ff01ddb9785b0b388077d74f4d319976920b8
Piorek1994 <pprus94@gmail.com>
2022-05-11 15:56:25 +0200

    Project finished

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,31 +2,31 @@ import React from 'react'
 
 class ImageCard extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
 
-        this.state = { spans: 0 }
+        this.state = { spans: 0 };
 
-		this.imageRef = React.createRef()
+		this.imageRef = React.createRef();
 	}
 
 	componentDidMount() {
-		this.imageRef.current.addEventListener('load', this.setSpans)
+		this.imageRef.current.addEventListener('load', this.setSpans);
 	}
 
 	setSpans = () => {
-        const height = this.imageRef.current.clientHeight
+        const height = this.imageRef.current.clientHeight;
 
-        const spans = Math.ceil(height / 10)
+        const spans = Math.ceil(height / 10);
 
-        this.setState({ spans })
+        this.setState({ spans });
 
     }
 
 	render() {
-		const { description, urls } = this.props.image
+		const { description, urls } = this.props.image;
 
 		return (
-			<div stlye={{ gridRowEnd: `span ${this.state.spans}`}}>
+			<div style={{ gridRowEnd: `span ${this.state.spans}`}}>
 				<img ref={this.imageRef} alt={description} src={urls.regular} />
 			</div>
 		)
